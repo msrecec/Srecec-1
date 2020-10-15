5e344172d9b43ee834839e2ec20bd7ca8899057d
Mislav Srecec <mislav.srecec3@gmail.com>
2020-10-15 13:56:46 +0200

    Finished all tasks up to the 10th task

--- src/hr/java/covidportal/model/Osoba.java
+++ src/hr/java/covidportal/model/Osoba.java
@@ -2,5 +2,55 @@ package hr.java.covidportal.model;
 
 public class Osoba {
     private String ime, prezime;
+    private Zupanija zupanija;
+    private Bolest zarazenBolescu;
+    private Osoba[] kontaktiraneOsobe;
 
+    public Osoba(String ime, String prezime, Zupanija zupanija, Bolest zarazenBolescu, Osoba[] kontaktiraneOsobe) {
+        this.ime = ime;
+        this.prezime = prezime;
+        this.zupanija = zupanija;
+        this.zarazenBolescu = zarazenBolescu;
+        this.kontaktiraneOsobe = kontaktiraneOsobe;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public Zupanija getZupanija() {
+        return zupanija;
+    }
+
+    public void setZupanija(Zupanija zupanija) {
+        this.zupanija = zupanija;
+    }
+
+    public Bolest getZarazenBolescu() {
+        return zarazenBolescu;
+    }
+
+    public void setZarazenBolescu(Bolest zarazenBolescu) {
+        this.zarazenBolescu = zarazenBolescu;
+    }
+
+    public Osoba[] getKontaktiraneOsobe() {
+        return kontaktiraneOsobe;
+    }
+
+    public void setKontaktiraneOsobe(Osoba[] kontaktiraneOsobe) {
+        this.kontaktiraneOsobe = kontaktiraneOsobe;
+    }
 }
